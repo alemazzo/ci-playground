d4b3d10b7dd3befcfeef8bc619630f6c434ef335
Alessandro Mazzoli <alessandro.mazzoli9@studio.unibo.it>
2024-02-08 17:20:18 +0100

    feat!: switched release method to semantic-release with proper configuration

---
+++ .github/workflows/release.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    branches: ["main"],
+    preset: "conventionalcommits",
+    plugins: [
+        "@semantic-release/commit-analyzer",
+        "@semantic-release/release-notes-generator",
+        "@semantic-release/changelog",
+        "@semantic-release/github",
+        "@semantic-release/git",
+    ],
+};
